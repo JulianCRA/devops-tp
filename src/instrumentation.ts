2f798acfcb1e244bb00f756582b1eed84b90d8e1
juliancra <juliancra@users.noreply.github.com>
2026-05-20 20:36:04 +0700

    dups?

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,14 +2,11 @@ import { NodeSDK } from '@opentelemetry/sdk-node'
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node'
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-http'
-import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http'
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics'
-import { SimpleLogRecordProcessor } from '@opentelemetry/sdk-logs'
 
 const sdk = new NodeSDK({
   traceExporter: new OTLPTraceExporter(),
   metricReader: new PeriodicExportingMetricReader({ exporter: new OTLPMetricExporter() }),
-  logRecordProcessor: new SimpleLogRecordProcessor(new OTLPLogExporter()),
   instrumentations: [getNodeAutoInstrumentations({
     '@opentelemetry/instrumentation-http': {
       ignoreIncomingRequestHook: (req) => req.url?.startsWith('/error/') ?? false,
