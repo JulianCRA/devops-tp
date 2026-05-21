32c23b73c5bb91ae4d34e749bcffc82c5a4629a0
juliancra <juliancra@users.noreply.github.com>
2026-05-21 16:27:07 +0700

    - actualizacion de env de ejemplo - print de version a consola

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import './instrumentation'
 import app from './app'
 import * as simulator from './trafficSimulator'
 import logger from './logger'
+import { version } from '../package.json'
 
 const PORT = process.env.PORT ?? 3000
 
 app.listen(PORT, () => {
   logger.info('Servidor corriendo', { port: PORT })
+  console.log(`Version del servidor: ${version}`)
 })
